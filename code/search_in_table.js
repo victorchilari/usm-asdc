f3da93342429d2ea39fd41c0d543f42302d7e18b
victorchilari <victor1chilari1@gmail.com>
2021-03-13 16:55:29 +0200

    [Lab1] Create binary tree

--- code/search_in_table.js
+++ code/search_in_table.js
@@ -1,4 +1,4 @@
-import {DATA as DATA} from './api.js';
+import {DATA_SHUFFLED as DATA} from './api.js';
 //_SHUFFLED
 function sequential(
 	arr,
@@ -16,7 +16,39 @@ function sequential(
 	return i; // number of iterations
 }
 
+function binaryTree(
+	array,
+	propName = 'id',
+	propValue = Math.floor(Math.random() * array.length)
+) {
+	const initValue = array[0][propName];
+	let node = {value: initValue, min: undefined, max: undefined};
+	// Create binary tree
+	for (let i = 1; i < array.length; i++) {
+		const number = array[i][propName];
+
+		let studyTree = node;
+		let isntInserted = true;
+		let state = '';
+		do {
+			if (number < studyTree.value) {
+				state = 'min';
+			} else {
+				state = 'max';
+			}
+			if (studyTree[state] == undefined) {
+				studyTree[state] = {value: number, min: undefined, max: undefined};
+				isntInserted = false;
+			} else {
+				studyTree = studyTree[state];
+			}
+		} while (isntInserted);
+	}
+	console.log(node);
+}
+
 const nodeArgs = process.argv.slice(2);
-console.log(DATA);
-sequential(DATA);
-console.log('Node args: ', nodeArgs);
+// console.log(DATA);
+// sequential(DATA);
+// console.log('Node args: ', nodeArgs);
+binaryTree(DATA);
