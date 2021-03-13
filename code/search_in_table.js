c27702af4b54f1b159b4a89632e69e2b49afd568
victorchilari <victor1chilari1@gmail.com>
2021-03-13 15:42:23 +0200

    [Lab1] sequential algorithm

--- code/search_in_table.js
+++ code/search_in_table.js
@@ -1,3 +1,22 @@
-import {DATA_SHUFFLED as DATA} from './api.js';
+import {DATA as DATA} from './api.js';
+//_SHUFFLED
+function sequential(
+	arr,
+	propName = 'id',
+	propValue = Math.floor(Math.random() * arr.length)
+) {
+	let instFound = true;
+	let i = 0;
+	do {
+		const element = arr[i];
+		if (element[propName] == propValue) instFound = false;
+		i++;
+	} while (instFound && i < arr.length);
+	// console.log(propValue, i, instFound, arr.length);
+	return i; // number of iterations
+}
 
+const nodeArgs = process.argv.slice(2);
 console.log(DATA);
+sequential(DATA);
+console.log('Node args: ', nodeArgs);
